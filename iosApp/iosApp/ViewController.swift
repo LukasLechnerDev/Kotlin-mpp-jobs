ee6684b43b85cfe390e8df93979ff3ae3a4c4a4b
Lukas Lechner <lukas.lechner@riag-digital.com>
2019-03-29 17:05:53 +0100

    Load jobs on iOS

--- iosApp/iosApp/ViewController.swift
+++ iosApp/iosApp/ViewController.swift
@@ -1,14 +1,33 @@
 import UIKit
 import main
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, JobsListView {
+    
+    @IBOutlet weak var label: UILabel!
+    
+    private lazy var api = GithubJobsApi()
+    private lazy var repository = JobPositionRepositoryImpl(apiService: api)
+    private lazy var presenter = JobsListPresenter(view: self,repository: repository)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        label.text = GithubJobsApi().fetchJobsSync()
+        presenter.getJobsList()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    @IBOutlet weak var label: UILabel!
+    
+    func showProgressIndicator(show: Bool) {
+        label.text = "loading"
+    }
+    
+    func showError(error: KotlinThrowable) {
+        label.text = "error"
+    }
+    
+    func getJobsListSuccess(jobs: [JobPositionDto]) {
+        label.text = "Success"
+        print(jobs[0].company)
+    }
 }
